9f21859a914db0091041f849052cf58a49c10fbb
Denis Chakyr <ChakyrIT@gmail.com>
2023-10-09 16:54:00 +0300

    added change of the themes inside App
    1) Added Notififcation for fully update the theme

---
+++ HSR - Guides and Tools/Helpers/Notitification.Name + Extens.swift
@@ -0,0 +1,12 @@
+//
+//  Notitification.Name + Extens.swift
+//  HSR - Guides and Tools
+//
+//  Created by Денис on 09.10.2023.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let didChangeTheme = Notification.Name("didChangeName")
+}
