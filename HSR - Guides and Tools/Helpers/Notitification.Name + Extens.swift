aad77a70a69ca69a07f84604b91e05b14a178e8b
Denis Chakyr <ChakyrIT@gmail.com>
2023-10-11 09:50:56 +0300

    Added Request for Score in AppStore

--- HSR - Guides and Tools/Helpers/Notitification.Name + Extens.swift
+++ HSR - Guides and Tools/Helpers/Notitification.Name + Extens.swift
@@ -9,4 +9,5 @@ import Foundation
 
 extension Notification.Name {
     static let didChangeTheme = Notification.Name("didChangeName")
+    static let showReviewAlert = Notification.Name("showReviewAlert")
 }
